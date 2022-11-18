5ca82824866ef46333cba98d84e4bb81ea58ce55
Luis Alexander Fernandez Garcia <afernandeznet@gmail.com>
2022-11-18 09:41:56 -0500

    Se agrega EventBus

---
+++ src/PeruTracking.Services.Notifications/Handlers/Customers/CustomerCreatedHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace PeruTracking.Services.Notifications.Handlers.Customers
+{
+    using MediatR;
+    using PeruTracking.Services.Notifications.Builders;
+    using PeruTracking.Services.Notifications.Messages.Events;
+    using PeruTracking.Services.Notifications.Services;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class CustomerCreatedHandler : AsyncRequestHandler<CustomerCreated>
+    {
+        private readonly MailKitOptions options;
+        private readonly IMessagesService messagesService;
+
+        public CustomerCreatedHandler(MailKitOptions options,
+            IMessagesService messagesService)
+        {
+            this.options = options;
+            this.messagesService = messagesService;
+        }
+
+        protected override Task Handle(CustomerCreated request, CancellationToken cancellationToken)
+        {
+            return Task.Run(async () =>
+            {
+                var message = MessageBuilder
+                .Create()
+                .WithReceiver(request.FirstName, request.Email)
+                .WithSender(options.Name, options.Email)
+                .WithSubject("Registro de Usuario - Confirmar Cuenta")
+                .WithBody("CustomerCreated",
+                        new
+                        {
+                            name = request.FirstName,
+                            url = "https://blog.christian-schou.dk/send-emails-with-asp-net-core-with-mailkit/"
+                        })
+                .Build();
+
+                await messagesService.SendAsync(message);
+
+            }, cancellationToken);
+        }
+    }
+}
---
+++ src/PeruTracking.Services.Notifications/Messages/Events/CustomerCreated.cs
@@ -0,0 +1,24 @@
+﻿namespace PeruTracking.Services.Notifications.Messages.Events
+{
+    using MediatR;
+    using Newtonsoft.Json;
+    using PeruTracking.Common.EventBus.Messaging;
+    using System;
+
+    public class CustomerCreated : IEvent, IRequest
+    {
+        public Guid Id { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Email { get; }
+
+        [JsonConstructor]
+        public CustomerCreated(Guid id, string firstName, string lastName, string email)
+        {
+            Id = id;
+            FirstName = firstName; 
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
